9556b0d8217d52ea23dd9dc0f263d7e7f06dac5d
marriet-aho <marrietaho2020@gmail.com>
2024-12-04 09:57:12 +0000

    adding all files

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ function checkGender() {
     let gender = prompt("Please enter your gender (Male or Female):");
 
     // Check the user's input and alert the appropriate message
-    if (gender.toLowerCase() === "female") {
+    if (gender.user() === "female") {
         alert("You are a girl!👧🏽");
-    } else if (gender.toLowerCase() === "male") {
+    } else if (gender.user() === "male") {
         alert("You are a boy! 👦🏽");
     } else {
         alert("Invalid input. Please enter 'Male' or 'Female'.");
